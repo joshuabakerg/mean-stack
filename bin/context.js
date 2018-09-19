92e33bd25b04491e81ff59d8f1a493aa608fdb19
Joshua <joshuab2@discovery.co.za>
2018-09-19 23:09:40 +0200

    add firebase storage show profile pic on mobile

--- bin/context.js
+++ bin/context.js
@@ -4,16 +4,26 @@ let UserService = require("./services/user.service");
 let ChatService = require("./services/chat.service");
 let EmailService = require("./services/email.service");
 
-function getAuth(){
+function getAuth() {
   let authJson = Buffer.from(process.env.APP_AUTH, 'base64').toString('ascii');
   return JSON.parse(authJson);
 }
 
-if(!global.context){
+if (!global.context) {
   global.context = {};
+
   global.context.auth = getAuth();
+
+  //Firebase
   console.log(JSON.stringify(global.context.auth, undefined, 2));
-  admin.initializeApp({credential: admin.credential.cert(global.context.auth.serviceAccount), databaseURL: "https://mean-angular.firebaseio.com"});
+  admin.initializeApp(
+    {
+      credential: admin.credential.cert(global.context.auth.serviceAccount),
+      databaseURL: "https://mean-angular.firebaseio.com",
+      storageBucket: "mean-angular.appspot.com"
+    }
+  );
+
   global.context.emailService = new EmailService('mail.joshuabakerg.co.za', 587, global.context.auth.emailAccount);
   global.context.userService = new UserService(global.context.emailService);
   global.context.chatService = new ChatService();
--- test.js
+++ test.js
@@ -4,11 +4,10 @@ var admin = require("firebase-admin");
 let authJson = Buffer.from(process.env.APP_AUTH, 'base64').toString('ascii');
 let auth = JSON.parse(authJson);
 
-var serviceAccount = require("./serviceAccountKey.json");
-serviceAccount.private_key = process.env.FIRE_BASE_PRIVATE_KEY.replace(/\\n/g, '\n');
 admin.initializeApp({
   credential: admin.credential.cert(auth.serviceAccount),
-  databaseURL: "https://mean-angular.firebaseio.com"
+  databaseURL: "https://mean-angular.firebaseio.com",
+  storageBucket: "mean-angular.appspot.com"
 });
 
 var db = admin.database();
@@ -20,6 +19,15 @@ db.ref("/user").once("value")
     let user = JSON.stringify(key);
     console.log(user)
   });
+
+var bucket = admin.storage().bucket();
+
+bucket.upload('./src/assets/images/unknown.png').then(data => {
+  console.log('upload success');
+  console.log(data);
+}).catch(err => {
+  console.log('error uploading to storage', err);
+});
 
 
 
