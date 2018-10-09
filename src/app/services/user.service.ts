d4a6d8c738a337275609a367a8a545c269d4dd73
Joshua <joshuab2@discovery.co.za>
2018-10-09 19:50:04 +0200

    add non committed files

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllUserNames(): Observable<any> {
+    return this.http.get('/services/user/usernames');
+  }
+
+}
